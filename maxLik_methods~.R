246998a6ae8efd86a561446595c621a916d2038a
Martim <martim@mik>
2020-10-15 13:07:18 +0000

    corrected

--- maxLik_methods~.R
+++ maxLik_methods~.R
@@ -1,25 +1,17 @@
-f <- function(x) {
-  res <-exp(x) + exp(-x) - 5 - x
-  return(res)
+f <- function(x){
+  return(exp(x) + exp(-x) - 5 - x)
 }
 
-expr <- expression(exp(x) + exp(-x) - 5 - x)
-
-first_deriv <- deriv(expr, namevec = "x", func = T)
-first_deriv(1)[1]
-
-
-e <- 1
-x_i <- 100
-iter <- 1
-
-while(e > 0.001 & iter < 100) {
-  x_i1 <- x_i - (f(x_i) / first_deriv(x_i)[1])
-  e <- abs(x_i1 - x_i)
-  cat('iter: ', iter, 'x_i: ', x_i, 'x_i+1: ', x_i1, 'Error: ', e, '\n', '---------------', '\n')
-  x_i <- x_i1
-  iter <- iter + 1
+f_deriv <- function(x) {
+  return(exp(x) - exp(-x) - 1)
 }
 
-library(maxLik)
-maxNR(f, start = 2)
+
+x0 <- 2
+a <- 's'
+while(a == 's'){
+  x1 <- x0 - f(x0)/f_deriv(x0)
+  x0 <- x1
+  print(x1)
+  a <- as.character(readline(prompt = 'Continue?: '))
+}
